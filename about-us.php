c17907aa671077c6c67279d775dd8150cbb6c68c
Hamza Khan <hamza.khan@technado.pk>
2021-12-17 23:00:57 +0500

    petroleum

---
+++ about-us.php
@@ -0,0 +1,130 @@
+<?php include 'includes/header.php';?>
+
+<div class="innerPageWrap">
+    
+    <section class="aboutBanner">
+        <div class="container">
+            <div class="innerBannerContent">
+                <p class="breadcrumbs"><span>Home</span> / <span class="active">About Us</span></p>
+                <p class="boldTitle">
+                    <span class="textWhite">About</span> <span class="textRed">US</span> 
+                </p>
+                <p class="desc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua
+                </p>
+            </div>
+        </div>
+        <img src="assets/images/about-banner-cards-2.png" alt="card" class="img-fluid aboutBannerCard" data-aos="fade-up" data-aos-duration="3000">
+    </section>
+
+    <section class="aboutSecTwo">
+        <div class="container">
+            <div class="row">
+                <div class="co-12 col-md-6 col-lg-6">
+                    <div class="rightContent">
+                        <p class="lightTitle lightItalicFont textBlack">About Us</p>
+                        <p class="boldTitle boldItalicFont"><span class="textBlack">GO RUN A SMARTER,</span> <span class="textRed">MORE EFFICIENT FLEET</span> </p>
+                        <p class="text normalText">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui, labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui, labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui.
+                        </p>
+                    </div>
+                </div>
+                <div class="co-12 col-md-6 col-lg-6">
+                    <div class="rightImgWrap">
+                        <img src="assets/images/about-fuel-station.png" alt="fuel station" class="img-fluid relImg">
+                        <img src="assets/images/about-extra-1.png" alt="card" class="img-fluid absImgMiddle" data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="1500"
+                        >
+                        <img src="assets/images/about-extra-2.png" alt="card" class="img-fluid absImgBottom" data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="2000"
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="aboutSecThree">
+        <div class="container">
+            <div class="row">
+                <div class="co-12 col-md-6 col-lg-6">
+                </div>
+                <div class="co-12 col-md-6 col-lg-6">
+                    <div class="rightContent">
+                        <p class="lightTitle lightItalicFont textBlack">About Us</p>
+                        <p class="boldTitle boldItalicFont"><span class="textBlack">GO RUN A SMARTER,</span> <span class="textRed">MORE EFFICIENT FLEET</span> </p>
+                        <p class="text normalText">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui, labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui, labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui.
+                        </p>
+                        <ul class="listing">
+                            <li><span><img src="assets/images/check.png" alt="check" class="img-fluid"></span> Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+                            <li><span><img src="assets/images/check.png" alt="check" class="img-fluid"></span> Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+                            <li><span><img src="assets/images/check.png" alt="check" class="img-fluid"></span> Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+                            <li><span><img src="assets/images/check.png" alt="check" class="img-fluid"></span> Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+                            <li><span><img src="assets/images/check.png" alt="check" class="img-fluid"></span> Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="assets/images/about-card-holder.png" alt="card" class="img-fluid bottomImg" data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine"
+        >
+    </section>
+
+
+    <section class="aboutSecFour">
+        <div class="container">
+            <div class="row">
+                <div class="co-12 col-md-6 col-lg-6">
+                    <div class="rightContent">
+                        <p class="lightTitle lightItalicFont textBlack">About Us</p>
+                        <p class="boldTitle boldItalicFont"><span class="textBlack">GO RUN A SMARTER,</span> <span class="textRed">MORE EFFICIENT FLEET</span> </p>
+                        <p class="text normalText">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui, labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui, labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliqui.
+                        </p>
+                    </div>
+                </div>
+                <div class="co-12 col-md-6 col-lg-6">
+                    <div class="rightImgWrap">
+                        <img src="assets/images/about-extras-4.png" alt="fuel station" class="img-fluid relImg">
+                        <img src="assets/images/about-extras-3.png" alt="card" class="img-fluid extraTop" data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="2000"
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+
+
+<?php include 'includes/footer.php';?>
---
+++ fuel-card.php
@@ -0,0 +1,152 @@
+<?php include 'includes/header.php';?>
+
+<div class="fuelCardWrap">
+
+    <section class="aboutBanner fuelBanner">
+        <div class="container">
+            <div class="innerBannerContent">
+                <p class="breadcrumbs"><span>Home</span> / <span class="active">Fuel Cards</span></p>
+                <p class="boldTitle">
+                    <span class="textWhite">FUEL</span> <span class="textRed">CARDS</span> 
+                </p>
+                <p class="desc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua
+                </p>
+            </div>
+        </div>
+        <img src="assets/images/fuel-banner-extras.png" alt="card" class="img-fluid fuelBannerCard" data-aos="fade-up" data-aos-duration="3000">
+    </section>
+
+    <section class="fuelCardSectionTwo">
+        <div class="container">
+            <div class="innerDesc">
+                <p class="boldTitle boldItalicFont"><span class="textBlack">Getting The</span> <span class="textRed">Card</span> </p>
+                <p class="desc text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, magna</p>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="cardProcess text-center" data-aos="fade-down" data-aos-easing="linear"  data-aos-duration="1500">
+                        <div class="icon">
+                            <img src="assets/images/card-process-icon-1.png" alt="icon" class="img-fluid">
+                        </div>
+                        <p class="title">Online Application</p>
+                        <p class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut aliqua.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="cardProcess text-center" data-aos="fade-down" data-aos-easing="linear"  data-aos-duration="2000">
+                        <div class="icon">
+                            <img src="assets/images/card-process-icon-1.png" alt="icon" class="img-fluid">
+                        </div>
+                        <p class="title">Get an Approval</p>
+                        <p class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut aliqua.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="cardProcess text-center" data-aos="fade-down" data-aos-easing="linear"  data-aos-duration="2500">
+                        <div class="icon">
+                            <img src="assets/images/card-process-icon-1.png" alt="icon" class="img-fluid">
+                        </div>
+                        <p class="title">Lorem Ipsum Dummy</p>
+                        <p class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut aliqua.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="fuelCardSectionThree">
+        <div class="container">
+            <div class="innerDesc">
+                <p class="boldTitle boldItalicFont"><span class="textBlack">Card</span> <span class="textRed">Protection</span> </p>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="leftCol">
+                        <div class="protectionDetails" data-aos="fade-right"
+                                data-aos-offset="200"
+                                data-aos-easing="ease-in-sine"
+                            >
+                            <p class="title">Pin Code</p>
+                            <p class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut aliqua.
+                            </p>
+                        </div>
+                        <div class="protectionDetails" data-aos="fade-right"
+                                data-aos-offset="400"
+                                data-aos-easing="ease-in-sine"
+                        >
+                            <p class="title">Lorem Ipsum</p>
+                            <p class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-6">
+                    <div class="centerCard text-center">
+                        <img src="assets/images/card-center.png" alt="card" class="img-fluid center" data-aos="fade-down" data-aos-easing="linear"  data-aos-duration="1500">
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="rightCol">
+                        <div class="protectionDetails" data-aos="fade-down"
+                                data-aos-easing="linear"
+                                data-aos-duration="1000"
+                        >
+                            <p class="title">Lorem Ipsum</p>
+                            <p class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut aliqua.
+                            </p>
+                        </div>
+                        <div class="protectionDetails" data-aos="fade-down"
+                                data-aos-easing="linear"
+                                data-aos-duration="1500"
+                        >
+                            <p class="title">Lorem Ipsum</p>
+                            <p class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button type="button" class="genBtnStyle">Fill Out An Application</button>
+        </div>
+    </section>
+
+    <section class="fuelCardSectionFour">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6 mb-4">
+                    <div class="innerDesc">
+                        <p class="boldTitle boldItalicFont"><span class="textBlack">Card</span> <span class="textRed">Protection</span> </p>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        </ul>
+                    </div>
+                    
+                </div>
+                <div class="col-12 col-md-6 col-lg-6 mb-4">
+                    <div class="rightImgWrap">
+                        <img src="assets/images/card-shadow.png" alt="card" class="img-fluid card-hold" data-aos="fade-down"
+                            data-aos-easing="linear"
+                            data-aos-duration="1500"
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php include 'includes/footer.php';?>
